e2be4cccaa611893f44d8c5aa7971e21f2481d55
Hungnth <thienhungnth@gmail.com>
2026-01-05 17:47:50 +0700

    feet: add example config file

---
+++ config-example.py
@@ -0,0 +1,22 @@
+# Laravel Herd Configuration File
+win_herd_path = r"F:\laravel-herd"
+win_herd_sites_path = r"F:\laravel-herd\sites"
+win_herd_bin_path = r"%USERPROFILE%\.config\herd\bin"
+
+mac_herd_path = r""
+mac_herd_sites_path = r""
+mac_herd_bin_path = r""
+
+admin_username = "admin"
+admin_password = "admin"
+admin_email = "admin@gmail.com"
+
+language = "vi"
+
+theme_apis = "themes.json"
+plugin_apis = "plugins.json"
+
+bulk_restore_path = "bulk_restore.csv"
+
+api_base_url = ""
+api_key = ""
