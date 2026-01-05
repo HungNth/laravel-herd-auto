0e7e61fdc74ca1d52858a7a3565df2f2fbc83cdb
Hungnth <thienhungnth@gmail.com>
2026-01-05 17:48:26 +0700

    feat: add main function for user input handling

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils.user_input import get_input
+
+async def main():
+    website_name = get_input("Enter website name (no space): ", required=True)
+    
+    
+
+if __name__ == '__main__':
+    main()
