c18249127383e18c886c119f07414cbca83c0be4
Hungnth <thienhungnth@gmail.com>
2026-01-06 19:24:36 +0700

    feat: simplify main function to directly delete websites without async

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import asyncio
-
-
-async def main():
-    from container import wp
-    latest_version = wp.wp_latest_version()
-    print(f"Latest WordPress version: {latest_version}")
+def main():
+    from wordpress.wp import wp
+    
+    wp.delete_websites()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
