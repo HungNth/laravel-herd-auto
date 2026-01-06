a952058fb979389b1a3176df7bfaace92781dcd8
Hungnth <thienhungnth@gmail.com>
2026-01-06 15:21:42 +0700

    feat: refactor wp_cli.py to use run_command for subprocess calls and add database export/import functionality

--- wordpress/wp_cli.py
+++ wordpress/wp_cli.py
@@ -1,8 +1,15 @@
+from datetime import datetime
+from pathlib import Path
+from utils.user_input import clean_input
+
+
 class WPCLI:
     def __init__(self):
         import config
         import utils.os_helper as os_helper
+        from utils.commands import run_command
         
+        self.run_command = run_command
         self.db_user = config.db_username
         self.db_password = config.db_password
         self.db_host = config.db_host if os_helper.is_windows() else '127.0.0.1'
@@ -15,19 +22,16 @@ class WPCLI:
         self.wp_options = config.wp_options
     
     def wp_version(self, path):
-        import subprocess
         command = f'"{self.wpcli}" core version --path="{path}"'
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout.strip()
+        result = self.run_command(command)
+        return result
     
-    def wp_core_download(self, path, version='latest'):
-        import subprocess
-        command = f'"{self.wpcli}" core download --path="{path}" --version="{version}"'
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout.strip()
+    def wp_core_download(self, path):
+        command = f'"{self.wpcli}" core download --path="{path}" --skip-content'
+        result = self.run_command(command)
+        return result
     
     def wp_config_create(self, path, db_name, db_prefix='wp_'):
-        import subprocess
         command = (
             f'"{self.wpcli}" config create '
             f'--dbname="{db_name}" '
@@ -37,22 +41,18 @@ class WPCLI:
             f'--dbprefix="{db_prefix}" '
             f'--path="{path}"'
         )
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout.strip()
+        result = self.run_command(command)
+        return result
     
     def wp_db_create(self, path):
-        import subprocess
         command = (
             f'"{self.wpcli}" db create '
             f'--path="{path}"'
         )
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout.strip()
+        result = self.run_command(command)
+        return result
     
     def wp_install(self, site_name, admin_user, admin_password, admin_email):
-        import subprocess
-        from utils.user_input import clean_input
-        
         admin_user = clean_input(admin_user) if clean_input(admin_user) != self.admin_username else self.admin_username
         admin_password = clean_input(admin_password) if clean_input(
             admin_password) != self.admin_password else self.admin_password
@@ -76,33 +76,74 @@ class WPCLI:
             f'--admin_email="{admin_email}" '
             f'--path="{path}"'
         )
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout.strip()
+        result = self.run_command(command)
+        return result
     
     def wp_config_set(self, path, key, value):
-        import subprocess
         command = (
             f'"{self.wpcli}" config set '
             f'"{key}" "{value}" '
             f'--path="{path}"'
         )
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout.strip()
+        result = self.run_command(command)
+        return result
     
     def wp_options_set(self, path):
-        import subprocess
-        
         for option in self.wp_options:
             command = (
                 f'"{self.wpcli}" {option} '
                 f'--path="{path}"'
             )
-            result = subprocess.run(command, shell=True, capture_output=True, text=True)
-            print(result.stdout.strip())
-            if result.stderr:
-                print(result.stderr)
+            self.run_command(command, output=False)
+    
+    def get_admin_id(self, path):
+        command = (
+            f'"{self.wpcli}" user list --field=ID '
+            f'--path="{path}"'
+        )
+        result = self.run_command(command, print_output=False)
+        
+        return result[0]
+    
+    def get_db_prefix(self, path):
+        command = (
+            f'"{self.wpcli}" db prefix '
+            f'--path="{path}"'
+        )
+        result = self.run_command(command, print_output=False)
+        
+        return result
+    
+    def export_db(self, path, export_path=None):
+        now = datetime.now()
+        formatted_datetime = now.strftime("%m-%d-%Y_%Hh%Mm%Ss")
+        site_name = Path(path).name
+        
+        if not export_path:
+            export_filename = f"{site_name}_db_backup_{formatted_datetime}.sql"
+            export_path = Path(path) / export_filename
+        else:
+            export_path = Path(export_path).resolve() / ".sql" if Path(export_path).is_dir() else Path(export_path)
+        
+        command = (
+            f'"{self.wpcli}" db export '
+            f'"{export_path}" '
+            f'--path="{path}"'
+        )
+        self.run_command(command)
+    
+    def import_db(self, path, db_path):
+        if not Path(db_path).is_file() and Path(db_path).suffix.lower() != '.sql' and not Path(db_path).exists():
+            raise FileNotFoundError(f"The specified database file does not exist or is not a .sql file: {db_path}")
+        
+        command = (
+            f'"{self.wpcli}" db export" '
+            f'"{db_path}" '
+            f'--path="{path}"'
+        )
+        self.run_command(command)
 
 
 if __name__ == '__main__':
     wpcli = WPCLI()
-    wpcli.wp_options_set(r'F:\laravel-herd\sites\astro-estates')
+    wpcli.export_db(r"F:\laravel-herd\sites\astro-estates")
