d5ab7aa9331164fda3e051d9cd50d1a795222a10
Hungnth <thienhungnth@gmail.com>
2026-01-07 18:01:38 +0700

    refactor: simplify input handling functions and remove unused code

--- utils/user_input.py
+++ utils/user_input.py
@@ -1,51 +1,39 @@
+from typing import Literal
+
+
 def get_input(prompt, default=None, required=False):
     while True:
         user_input = input(prompt)
-        cleaned_input = clean_input(user_input)
         if not user_input and default is not None:
             return default
         if required and not user_input:
             print(f"This field is required. Please enter a value.")
             continue
-        return cleaned_input
+        return user_input
 
 
 def clean_input(user_input):
     return user_input.replace(" ", "-").lower()
 
 
-def get_admin_credentials():
-    import config
-    from wordpress.wp import wp
-    
-    admin_username = config.admin_username
-    admin_password = config.admin_password
-    admin_email = config.admin_email
-    
-    site_name = get_input('Enter the site name: ', required=True)
-    if wp.is_website_exists(site_name):
-        print(f'Site "{site_name}" already exists. Please choose a different name.')
-        return get_admin_credentials()
-    
-    is_default = get_input(f'Do you want to use default admin credentials? (y/n): ', default='y')
-    
-    if is_default != 'y':
-        admin_name_input = get_input(f'Enter the admin username "{admin_username}": ', default=admin_username,
-                                     required=True)
-        admin_pass_input = get_input(f'Enter the admin password "{admin_password}":', default=admin_password,
-                                     required=True)
-        admin_email_input = get_input(f'Enter the admin email "{admin_email}": ', default=admin_email, required=True)
-    else:
-        admin_name_input = admin_username
-        admin_pass_input = admin_password
-        admin_email_input = admin_email
-    
-    return site_name, admin_name_input, admin_pass_input, admin_email_input
+def clean_selection(user_input):
+    return user_input.replace(" ,", ",").replace(", ", ",").replace(" ", ",").split(",")
 
 
-def get_confirmation():
+def get_confirmation(prompt, default: Literal[True, False] = None):
+    valid_yes = {'y', 'yes'}
+    valid_no = {'n', 'no'}
+    
     while True:
-        confirmation = get_input("Are you sure you want to proceed? (y/n): ", required=True)
-        if confirmation in ['y', 'n']:
-            return confirmation == 'y'
+        choice = input(prompt).strip().lower()
+        
+        if choice == '' and default is not None:
+            return default
+        
+        if choice in valid_yes:
+            return True
+        
+        if choice in valid_no:
+            return False
+        
         print("Invalid input. Please enter 'y' or 'n'.")
