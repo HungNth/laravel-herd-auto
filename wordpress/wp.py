186ff6f3c19a49a8d5344feefad3bd7b51b804ba
Hungnth <thienhungnth@gmail.com>
2026-01-07 18:01:49 +0700

    feat: enhance WordPress class with improved admin credential handling and configuration options

--- wordpress/wp.py
+++ wordpress/wp.py
@@ -1,26 +1,94 @@
 import shutil
+from typing import Literal
 
+import config
 from utils.os_helper import herd_path
-from utils.user_input import get_admin_credentials, get_confirmation
+from utils.user_input import get_input, clean_input, get_confirmation
+from utils.herd import add_ssl
 
 
 class WordPress:
-    def __init__(self):
+    def __init__(self, wpc_li, wp_api, mysql):
         self.herd_sites_path, _, _ = herd_path()
+        self.wp_cli = wpc_li
+        self.wp_api = wp_api
+        self.mysql = mysql
+    
+    def get_admin_credentials(self):
+        admin_username = config.admin_username
+        admin_password = config.admin_password
+        admin_email = config.admin_email
+        
+        site_name = get_input('Enter the site name: ', required=True)
+        site_name = clean_input(site_name)
+        
+        if self.is_website_exists(site_name):
+            print(f'Site "{site_name}" already exists. Please choose a different name.')
+            return self.get_admin_credentials()
+        if self.mysql.check_database_exists(site_name):
+            print(f'Database for site "{site_name}" already exists. Please choose a different name.')
+            return self.get_admin_credentials()
+        
+        is_default = get_confirmation('Do you want to use default admin credentials? (Y/n): ', default=True)
+        
+        if is_default is False:
+            admin_username_input = get_input(f'Enter the admin username "{admin_username}": ', default=admin_username,
+                                             required=True)
+            admin_pass_input = get_input(f'Enter the admin password "{admin_password}":', default=admin_password,
+                                         required=True)
+            admin_email_input = get_input(f'Enter the admin email "{admin_email}": ', default=admin_email,
+                                          required=True)
+        else:
+            admin_username_input = admin_username
+            admin_pass_input = admin_password
+            admin_email_input = admin_email
+        
+        return site_name, admin_username_input, admin_pass_input, admin_email_input
+    
+    def get_setup_options(self):
+        selected_themes = ""
+        selected_plugins = []
+        
+        is_install_theme = get_confirmation(
+            'Do you want to install another themes? (Default theme: "Flatsome") (y/N): ',
+            default=False)
+        if is_install_theme:
+            selected_themes = self.wp_api.select_packages("theme")
+        
+        is_install_plugins = get_confirmation('Do you want to install plugins? (y/N): ', default=False)
+        if is_install_plugins:
+            selected_plugins = self.wp_api.select_packages("plugin")
+        
+        is_setup_wp_config = get_confirmation('Do you want to setup WordPress options? (Y/n): ', default=True)
+        
+        return selected_themes, selected_plugins, is_setup_wp_config
     
     def create_website(self):
+        site_name, admin_username, admin_password, admin_email = self.get_admin_credentials()
+        selected_themes, selected_plugins, is_setup_wp_config = self.get_setup_options()
         
-        from wordpress.wp_cli import wpcli
-        site_name, admin_name_input, admin_pass_input, admin_email_input = get_admin_credentials()
-        wpcli.wp_install(site_name, admin_name_input, admin_pass_input, admin_email_input)
+        site_path = self.herd_sites_path / site_name
+        
+        self.wp_cli.wp_install(site_name, admin_username, admin_password, admin_email)
+        if len(selected_themes) == 0:
+            selected_themes = [config.default_theme_slug]
+            self.install_packages(site_path, selected_themes, item_type="theme")
+        
+        if len(selected_plugins) > 0:
+            self.install_packages(site_path, selected_plugins, item_type="plugin")
+        if is_setup_wp_config:
+            self.wp_cli.wp_options_set(site_path)
+        
+        add_ssl(site_path)
     
     def delete_websites(self):
-        selected_sites = self.choose_website()
+        selected_sites = self.select_website()
         if not selected_sites:
             return
         
         print(f'You have selected the following websites for deletion: {", ".join(selected_sites)}')
-        if not get_confirmation():
+        if not get_confirmation("Are you sure you want to delete these websites? This action cannot be undone. (y/N): ",
+                                default=False):
             print("Deletion cancelled by user.")
             return
         
@@ -28,17 +96,27 @@ class WordPress:
             self.delete_website(site)
     
     def delete_website(self, site_name):
-        from db.mysql import mysql
         site_path = self.herd_sites_path / site_name
         
         if site_path.exists():
             try:
                 shutil.rmtree(site_path)
-                mysql.drop_database(site_name)
+                self.mysql.drop_database(site_name)
                 print(f'Website "{site_name}" and its database have been deleted successfully.\n')
             except Exception as e:
                 print(f'Error deleting website "{site_name}": {e}')
                 return
+    
+    def is_website_exists(self, site_name):
+        site_path = self.herd_sites_path / site_name
+        return site_path.exists()
+    
+    def get_site_list(self):
+        site_list = []
+        for site in self.herd_sites_path.iterdir():
+            if site.is_dir():
+                site_list.append(site.name)
+        return site_list
     
     def print_site_list(self):
         site_list = self.get_site_list()
@@ -52,18 +130,9 @@ class WordPress:
                 continue
             print(f"{index}. {site}")
     
-    def is_website_exists(self, site_name):
-        site_path = self.herd_sites_path / site_name
-        return site_path.exists()
-    
-    def get_site_list(self):
-        site_list = []
-        for site in self.herd_sites_path.iterdir():
-            if site.is_dir():
-                site_list.append(site.name)
-        return site_list
-    
-    def choose_website(self):
+    def select_website(self):
+        from utils.user_input import clean_selection
+        
         site_list = self.get_site_list()
         selected_websites = []
         
@@ -71,21 +140,21 @@ class WordPress:
         
         while True:
             try:
-                site_index = input(
-                    "Enter the website numbers you want to select\n"
-                    "(separated by commas, for example: 1,2,3 or 0 to select all): "
-                ).replace(" ", ",").split(",")
+                print(
+                    "Enter the website numbers you want to select, (e.g., 1 2 3 or 1,2,3). Enter 0 to select all websites.")
+                site_index = input("Your selection: ")
                 
-                site_index = [int(x) for x in site_index]
+                cleaned_site_index = clean_selection(site_index)
+                cleaned_site_index = [int(x) for x in cleaned_site_index]
                 
-                if 0 in site_index:
+                if 0 in cleaned_site_index:
                     return site_list
                 
-                for index in site_index:
+                for index in cleaned_site_index:
                     if 1 <= index <= len(site_list):
                         selected_websites.append(site_list[index - 1])
                     else:
-                        print(f"Please enter a number from 1 to {len(site_list)}!")
+                        print(f"Please enter a number from 1 to {len(site_list)}!\n")
                         break
                 else:
                     break
@@ -94,6 +163,60 @@ class WordPress:
                 print("Please enter a number!")
         
         return selected_websites
+    
+    def install_packages(self, path, selected_pkg_slugs, item_type: Literal["theme", "plugin"]):
+        if selected_pkg_slugs is None:
+            return
+        
+        selected_pkg_urls = self.wp_api.get_download_urls(selected_pkg_slugs)
+        
+        if item_type == "theme":
+            self.wp_cli.install_themes(selected_pkg_urls, path)
+        elif item_type == "plugin":
+            self.wp_cli.install_plugins(selected_pkg_urls, path)
+    
+    def reset_admin_info(self, selected_website):
+        for site in selected_website:
+            path = self.herd_sites_path / site
+            print(f'Resetting admin information for site: "{site}"')
+            self.mysql.change_username(site, config.admin_username)
+            self.mysql.change_password(site, config.admin_password)
+            self.wp_cli.update_admin_email(path, config.admin_email)
+            self.mysql.change_email(site, config.admin_email)
+    
+    def setup_wp_options(self, selected_website):
+        for site in selected_website:
+            path = self.herd_sites_path / site
+            print(f'Setting up WordPress options for site: "{site}"')
+            self.wp_cli.wp_options_set(path)
+    
+    def configure_wp(self):
+        options = [
+            '1. Reset Admin information to default',
+            '2. Setup WordPress options',
+            '3. Both 1 and 2',
+            '4. Exit'
+        ]
+        print("Select an option to configure WordPress:")
+        for option in options:
+            print(option)
+        choice = get_input("Your choice (1-4): ", required=True)
+        if choice == '4':
+            exit(0)
+        
+        if choice not in ['1', '2', '3']:
+            print("Invalid choice. Please select a valid option.")
+            return self.configure_wp()
+        
+        selected_website = self.select_website()
+        
+        if choice == '1':
+            self.reset_admin_info(selected_website)
+        elif choice == '2':
+            self.setup_wp_options(selected_website)
+        elif choice == '3':
+            self.reset_admin_info(selected_website)
+            self.setup_wp_options(selected_website)
+        return None
 
-
-wp = WordPress()
+# wp = WordPress()
