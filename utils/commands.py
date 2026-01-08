2752736e0b4d4d3e940bd1517b2cd9e49418d0b2
Hungnth <thienhungnth@gmail.com>
2026-01-08 10:40:06 +0700

    feat: add support for PowerShell commands in run_command function

--- utils/commands.py
+++ utils/commands.py
@@ -2,7 +2,16 @@ import subprocess
 from typing import Union, List
 
 
-def run_command(command, capture=True, print_output=True, shell=True):
+def run_command(command, capture=True, print_output=True, shell=True, pwsh=False):
+    if pwsh:
+        shell = False
+        command = [
+            "pwsh",
+            "-NoProfile",
+            "-Command",
+            command
+        ]
+    
     result = subprocess.run(
         command,
         shell=shell,
@@ -18,38 +27,6 @@ def run_command(command, capture=True, print_output=True, shell=True):
     
     return result.stdout.strip()
 
-# def run_command(
-#         args: Union[str, List[str]],
-#         *,
-#         shell: bool = False,
-#         capture: bool = True,
-#         print_output: bool = True,
-# ):
-#     if shell:
-#         if not isinstance(args, str):
-#             raise ValueError("args must be str when shell=True")
-#         cmd = args
-#     else:
-#         if not isinstance(args, (list, tuple)):
-#             raise ValueError("args must be list/tuple when shell=False")
-#
-#         cmd = []
-#         for a in args:
-#             if a is None:
-#                 continue
-#             cmd.append(str(a))
-#
-#     result = subprocess.run(
-#         cmd,
-#         shell=shell,
-#         capture_output=capture,
-#         text=True,
-#     )
-#
-#     if result.stderr:
-#         print(result.stderr.strip())
-#
-#     if print_output and result.stdout:
-#         print(result.stdout.strip())
-#
-#     return result.stdout.strip()
+# if __name__ == '__main__':
+#     cmd = 'ls'
+#     run_command(cmd, pwsh=True)
