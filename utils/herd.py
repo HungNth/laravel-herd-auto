bb24bfcf3375c24666640581845a1a90d739f41a
Hungnth <thienhungnth@gmail.com>
2026-01-08 10:12:57 +0700

    feat: add function to check if Herd application is running and handle errors

--- utils/herd.py
+++ utils/herd.py
@@ -5,3 +5,20 @@ def add_ssl(path):
     command = f'cd /d "{path}" && herd secure'
     subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True, stderr=subprocess.DEVNULL)
     print('SSL certificate added successfully.')
+
+
+def herd_is_open():
+    try:
+        result = subprocess.run('herd help', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        text = 'The Herd Desktop application is not running. Please start Herd and try again.'
+        if text in result.stdout.strip():
+            print(text)
+            return False
+        return True
+    except FileNotFoundError:
+        print('Herd command not found. Please ensure Herd is installed and accessible in your PATH.')
+        return False
+
+# if __name__ == '__main__':
+#     connection = check_herd_opened()
+#     print(connection)
