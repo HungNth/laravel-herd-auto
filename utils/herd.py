c6be702b5d6da16341f7d393246e1c71d73ee057
Hungnth <thienhungnth@gmail.com>
2026-01-08 21:22:25 +0700

    refactor: rename is_herd_open function to is_herd_running for clarity

--- utils/herd.py
+++ utils/herd.py
@@ -7,7 +7,7 @@ def add_ssl(path):
     print('SSL certificate added successfully.')
 
 
-def is_herd_open():
+def is_herd_running():
     try:
         result = subprocess.run('herd help', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         text = 'The Herd Desktop application is not running. Please start Herd and try again.'
