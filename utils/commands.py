708482ebc8316e9ce800fc1979099ed02d943489
Hungnth <thienhungnth@gmail.com>
2026-01-08 09:50:52 +0700

    feat: simplify run_command function for improved clarity and functionality

--- utils/commands.py
+++ utils/commands.py
@@ -2,44 +2,48 @@ import subprocess
 from typing import Union, List
 
 
-# def run_command(command, output=True, print_output=True):
-#     result = subprocess.run(command, shell=True, capture_output=True, text=True)
-#     if result.stderr:
-#         print(result.stderr)
-#     if print_output:
-#         print(result.stdout.strip())
-#     if output:
-#         return result.stdout.strip()
-#     return None
-
-
-def run_command(
-    args: Union[str, List[str]],
-    *,
-    shell: bool = False,
-    capture: bool = True,
-    print_output: bool = True,
-):
-    if shell:
-        if not isinstance(args, str):
-            raise ValueError("args must be str when shell=True")
-        cmd = args
-    else:
-        if not isinstance(args, (list, tuple)):
-            raise ValueError("args must be list/tuple when shell=False")
-        cmd = args
-
-    result = subprocess.run(
-        cmd,
-        shell=shell,
-        capture_output=capture,
-        text=True,
-    )
-
+def run_command(command, output=True, print_output=True, shell=True):
+    result = subprocess.run(command, shell=shell, capture_output=True, text=True)
     if result.stderr:
-        print(result.stderr.strip())
-
-    if print_output and result.stdout:
+        print(result.stderr)
+    if print_output:
         print(result.stdout.strip())
+    if output:
+        return result.stdout.strip()
+    return None
 
-    return result.stdout.strip()
+# def run_command(
+#         args: Union[str, List[str]],
+#         *,
+#         shell: bool = False,
+#         capture: bool = True,
+#         print_output: bool = True,
+# ):
+#     if shell:
+#         if not isinstance(args, str):
+#             raise ValueError("args must be str when shell=True")
+#         cmd = args
+#     else:
+#         if not isinstance(args, (list, tuple)):
+#             raise ValueError("args must be list/tuple when shell=False")
+#
+#         cmd = []
+#         for a in args:
+#             if a is None:
+#                 continue
+#             cmd.append(str(a))
+#
+#     result = subprocess.run(
+#         cmd,
+#         shell=shell,
+#         capture_output=capture,
+#         text=True,
+#     )
+#
+#     if result.stderr:
+#         print(result.stderr.strip())
+#
+#     if print_output and result.stdout:
+#         print(result.stdout.strip())
+#
+#     return result.stdout.strip()
