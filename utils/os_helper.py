1e040a36c67df50dd4bac6c16f31686211ea920a
Hungnth <thienhungnth@gmail.com>
2026-01-09 17:45:03 +0700

    refactor: enhance path handling by expanding user directories in os_helper.py

--- utils/os_helper.py
+++ utils/os_helper.py
@@ -28,18 +28,18 @@ def herd_path():
         from config import win_herd_sites_path, win_herd_cached_path, win_herd_bin_path
         if not win_herd_sites_path or not win_herd_cached_path or not win_herd_bin_path:
             return exit("Please configure the herd paths in 'config.py' for Windows.")
-        herd_sites_path = Path(win_herd_sites_path)
-        herd_cached_path = Path(win_herd_cached_path)
-        herd_bin_path = Path(win_herd_bin_path)
+        herd_sites_path = Path(win_herd_sites_path).expanduser().resolve()
+        herd_cached_path = Path(win_herd_cached_path).expanduser().resolve()
+        herd_bin_path = Path(win_herd_bin_path).expanduser().resolve()
         
         return herd_sites_path, herd_cached_path, herd_bin_path
     elif is_mac():
         from config import mac_herd_sites_path, mac_herd_cached_path, mac_herd_bin_path
         if not mac_herd_sites_path or not mac_herd_cached_path or not mac_herd_bin_path:
             return exit("Please configure the herd paths in 'config.py' for MacOs.")
-        herd_sites_path = Path(mac_herd_sites_path)
-        herd_cached_path = Path(mac_herd_cached_path)
-        herd_bin_path = Path(mac_herd_bin_path)
+        herd_sites_path = Path(mac_herd_sites_path).expanduser().resolve()
+        herd_cached_path = Path(mac_herd_cached_path).expanduser().resolve()
+        herd_bin_path = Path(mac_herd_bin_path).expanduser().resolve()
         
         return herd_sites_path, herd_cached_path, herd_bin_path
     else:
@@ -47,7 +47,3 @@ def herd_path():
 
 # if __name__ == '__main__':
 #     herd_sites_path, herd_cached_path, herd_bin_path = herd_path()
-#
-#     print(herd_sites_path)
-#     print(herd_cached_path)
-#     print(herd_bin_path)
