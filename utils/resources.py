75804a6cc97b6eaf67465dd484de11d8fa1267f4
Hungnth <thienhungnth@gmail.com>
2026-01-14 17:25:16 +0700

    feat: removed

--- utils/resources.py
+++
@@ -1,7 +0,0 @@
-class Resources:
-    def __init__(self):
-        import os_helper
-        self.herd_sites_path, self.herd_cached_path, self.herd_bin_path = os_helper.herd_path()
-    
-    def wp_local_version(self):
-        pass
